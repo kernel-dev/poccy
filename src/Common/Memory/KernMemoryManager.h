a4574131ad2a374987ad921124f3ac325c8893fb
kernel-dev <twitchisadeck@gmail.com>
2023-06-13 05:33:15 +0200

    draft(MemoryManagement): bitmap representation of physical frames

--- src/Common/Memory/KernMemoryManager.h
+++ src/Common/Memory/KernMemoryManager.h
@@ -10,29 +10,20 @@
 //
 //  Obtains the previous memory descriptor.
 //
-#define PREVIOUS_MEMORY_DESCRIPTOR(MEMORY, SIZE) ( \
+#define PREVIOUS_MEMORY_DESCRIPTOR(MEMORY, SIZE)  (\
     (EFI_MEMORY_DESCRIPTOR *)( \
         (UINT8 *)(MEMORY) - (SIZE) \
     ) \
 )
 
 /**
-    A bitmap of system memory,
-    where each region is marked
-    as free or unavailable.
- **/
-typedef struct {
-    BOOLEAN Free: 1;
-} MEMORY_BITMAP;
-
-/**
     Structure defining a physical memory page.
  **/
 typedef struct {
-    EFI_PHYSICAL_ADDRESS    Address;        // Beginning physical address of this page.
-    UINT32                  Size;           // Size of this page.
-    VOID                    *Buffer;        // The data this page holds.
-    BOOLEAN                 Free;           // Whether or not this page is free to use.
+  EFI_PHYSICAL_ADDRESS    Address;          // Beginning physical address of this page.
+  UINT32                  Size;             // Size of this page.
+  VOID                    *Buffer;          // The data this page holds.
+  BOOLEAN                 Free;             // Whether or not this page is free to use.
 } KERN_PHYSICAL_PAGE;
 
 /**
@@ -40,10 +31,10 @@ typedef struct {
     of a process's virtual memory space.
  **/
 typedef struct {
-    KERN_PHYSICAL_PAGE      *Pages;         // An array of mapped physical pages for this VM.
-    UINT32                  NumOfPages;     // The number of pages allocated for this VM.
-    UINT32                  SizeOfPage;     // The size of each physical page.
-    UINT8                   ProcessId;      // The identification value of the process.
+  KERN_PHYSICAL_PAGE    *Pages;             // An array of mapped physical pages for this VM.
+  UINT32                NumOfPages;         // The number of pages allocated for this VM.
+  UINT32                SizeOfPage;         // The size of each physical page.
+  UINT8                 ProcessId;          // The identification value of the process.
 }   InternalVMBlock;
 
 /**
@@ -51,9 +42,14 @@ typedef struct {
     page faults.
  **/
 enum PageFaultReason {
-    OutOfMemory,        // Out of memory
-    InvalidAccess,      // Process accessed an invalid memory page.
-    InternalError       // Something went wrong internally.
+  OutOfMemory,          // Out of memory
+  InvalidAccess,        // Process accessed an invalid memory page.
+  InternalError         // Something went wrong internally.
 };
+
+VOID
+KernCreateMMap (
+  IN EFI_KERN_MEMORY_MAP  *MemoryMap
+  );
 
 #endif /* KernMemoryManager.h */
--- src/Kernel/Memory/KernMemoryManager.c
+++ src/Kernel/Memory/KernMemoryManager.c
@@ -1,7 +1,51 @@
 #include "../../Common/Memory/KernMem.h"
+#include "../../Common/Memory/KernEfiMem.h"
 #include "../../Common/Memory/KernMemoryManager.h"
+#include "../../Common/Assert/KernAssert.h"
 #include "../../Common/Util/KernRuntimeValues.h"
+#include "../../Common/Util/KernString.h"
+#include "../../Common/Drivers/IO/serial.h"
 
 #include <Uefi.h>
 
 #include <Library/UefiLib.h>
+
+//
+//  FIXME:  Not all physical frames are mapped???
+//
+VOID
+KernCreateMMap (
+  IN EFI_KERN_MEMORY_MAP  *MemoryMap
+  )
+{
+  ASSERT (MemoryMap != NULL);
+  ASSERT (MemoryMap->MemoryMap != NULL);
+
+  EFI_MEMORY_DESCRIPTOR  *CurrDesc = MemoryMap->MemoryMap;
+  UINT8                  Bit = 0;
+
+  do {
+    if (CurrDesc->Type == EfiConventionalMemory) {
+      for (UINTN Index = 0; Index < CurrDesc->NumberOfPages; Index++) {
+        if (Bit == 8) {
+          Bit = 0;
+        }
+
+        Bitmap[BitmapSize] |= (1 << Bit);
+        Bit++;
+      }
+    }
+
+    Bitmap[BitmapSize] = 0x00;
+
+    BitmapSize++;
+
+    CurrDesc = (EFI_MEMORY_DESCRIPTOR *)NEXT_MEMORY_DESCRIPTOR (
+                                          CurrDesc,
+                                          MemoryMap->DescriptorSize
+                                          );
+  } while (
+    CurrDesc->PhysicalStart < (MemoryMap->MemoryMap + MemoryMap->MemoryMapSize)->PhysicalStart &&
+    CurrDesc->PhysicalStart < MEMORY_UPPER_BOUNDARY
+  );
+}
