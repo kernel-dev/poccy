8cf8f31132aaac59cfc5d2822e6013345446b4f7
kernel-dev <twitchisadeck@gmail.com>
2023-06-13 05:36:17 +0200

    add(KernMem): 16-bit and 8-bit memset

--- src/Kernel/Memory/KernMem.c
+++ src/Kernel/Memory/KernMem.c
@@ -5,84 +5,146 @@
 #include <Library/UefiLib.h>
 
 VOID *
-KernMemset32 (
-    IN  VOID        *PTR,
-    IN  INT32       Value,
-    IN  UINT32      Count
-)
+KernMemset8 (
+  IN  VOID   *PTR,
+  IN  INT8   Value,
+  IN  UINT8  Count
+  )
 {
-    UINT32 *Dest = PTR;
+  UINT8  *Dest = PTR;
 
-    while (Count--)
-    {
-        *Dest++ = (UINT32) Value;
-    }
+  while (Count--) {
+    *Dest++ = (UINT8)Value;
+  }
 
-    return PTR;
+  return PTR;
+}
+
+VOID *
+KernMemset16 (
+  IN  VOID    *PTR,
+  IN  INT16   Value,
+  IN  UINT16  Count
+  )
+{
+  UINT16  *Dest = PTR;
+
+  while (Count--) {
+    *Dest++ = (UINT16)Value;
+  }
+
+  return PTR;
+}
+
+VOID *
+KernMemset32 (
+  IN  VOID    *PTR,
+  IN  INT32   Value,
+  IN  UINT32  Count
+  )
+{
+  UINT32  *Dest = PTR;
+
+  while (Count--) {
+    *Dest++ = (UINT32)Value;
+  }
+
+  return PTR;
 }
 
 VOID *
 KernMemset64 (
-    IN  VOID        *PTR,
-    IN  INT64       Value,
-    IN  UINT64      Count
-)
+  IN  VOID    *PTR,
+  IN  INT64   Value,
+  IN  UINT64  Count
+  )
 {
-    UINT64 *Dest = PTR;
+  UINT64  *Dest = PTR;
 
-    while (Count--)
-    {
-        *Dest++ = (UINT64) Value;
-    }
+  while (Count--) {
+    *Dest++ = (UINT64)Value;
+  }
 
-    return PTR;
+  return PTR;
+}
+
+VOID *
+VolatileKernMemset8 (
+  IN  VOID   *PTR,
+  IN  INT8   Value,
+  IN  UINT8  Count
+  )
+{
+  volatile UINT8  *Dest = PTR;
+
+  while (Count--) {
+    *Dest++ = (UINT8)Value;
+  }
+
+  return PTR;
+}
+
+VOID *
+VolatileKernMemset16 (
+  IN  VOID    *PTR,
+  IN  INT16   Value,
+  IN  UINT16  Count
+  )
+{
+  volatile UINT16  *Dest = PTR;
+
+  while (Count--) {
+    *Dest++ = (UINT16)Value;
+  }
+
+  return PTR;
 }
 
 VOID *
 VolatileKernMemset32 (
-    IN  VOID        *PTR,
-    IN  INT32       Value,
-    IN  UINT32      Count
-)
+  IN  VOID    *PTR,
+  IN  INT32   Value,
+  IN  UINT32  Count
+  )
 {
-    volatile UINT32 *Dest = PTR;
+  volatile UINT32  *Dest = PTR;
 
-    while (Count--)
-    {
-        *Dest++ = (UINT32) Value;
-    }
+  while (Count--) {
+    *Dest++ = (UINT32)Value;
+  }
 
-    return PTR;
+  return PTR;
 }
 
 VOID *
 VolatileKernMemset64 (
-    IN  VOID        *PTR,
-    IN  INT64       Value,
-    IN  UINT64      Count
-)
+  IN  VOID    *PTR,
+  IN  INT64   Value,
+  IN  UINT64  Count
+  )
 {
-    volatile UINT64 *Dest = PTR;
+  volatile UINT64  *Dest = PTR;
 
-    while (Count--)
-    {
-        *Dest++ = (UINT64) Value;
-    }
+  while (Count--) {
+    *Dest++ = (UINT64)Value;
+  }
 
-    return PTR;
+  return PTR;
 }
 
 VOID
 KernCopyMem (
-    IN  VOID    *Destination,
-    IN  VOID    *Source,
-    IN  UINTN   Size)
+  IN  VOID   *Destination,
+  IN  VOID   *Source,
+  IN  UINTN  Size
+  )
 {
-    UINTN *Dest = (UINTN *)Destination;
-    UINTN *Src  = (UINTN *)Source;
+  UINTN  *Dest = (UINTN *)Destination;
+  UINTN  *Src  = (UINTN *)Source;
 
-    for (UINTN Index = 0; Index < Size; Index++)
-        Dest[Index] = Src[Index];
+  for (UINTN Index = 0; Index < Size; Index++) {
+    Dest[Index] = Src[Index];
+  }
 }
 
 // VOID *
