38becd8bdce982f4278e459319db724ac5a8bf1b
kernel-dev <twitchisadeck@gmail.com>
2023-06-13 05:37:49 +0200

    feat(IO): `InB8` & `OutB8`

--- src/Kernel/Drivers/IO/io.c
+++ src/Kernel/Drivers/IO/io.c
@@ -3,3 +3,29 @@
 #include <Uefi.h>
 
 #include <Library/UefiLib.h>
+
+VOID
+OutB8 (
+  IN UINT16  Port,
+  IN UINT8   Value
+  )
+{
+  asm volatile (
+      "outb %0, %1" : : "a"(Value), "Nd"(Port) : "memory"
+  );
+}
+
+UINT8
+InB8 (
+  IN UINT16  Port
+  )
+{
+  UINT8  Ret;
+
+  asm volatile ( "inb %1, %0"
+                 : "=a"(Ret)
+                 : "Nd"(Port)
+                 : "memory");
+
+  return Ret;
+}
