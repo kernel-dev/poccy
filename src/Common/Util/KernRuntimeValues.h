d0dbf6fa9440fb08e192340a32d8c01e96a684be
kernel-dev <twitchisadeck@gmail.com>
2023-06-13 05:37:23 +0200

    refactor(RuntimeVals): use stack allocation for bitmap
    - At least until a proper `kmalloc` and `krealloc` are implemented..

--- src/Common/Util/KernRuntimeValues.h
+++ src/Common/Util/KernRuntimeValues.h
@@ -14,37 +14,37 @@
 //
 //  Screen row.
 //
-extern UINT32                   ScreenRow;
+extern UINT32  ScreenRow;
 
 //
 //  Screen column.
 //
-extern UINT32                   ScreenCol;
+extern UINT32  ScreenCol;
 
 //
 //  KERN_FRAMEBUFFER pointer.
 //
-extern KERN_FRAMEBUFFER         *FB;
+extern KERN_FRAMEBUFFER  *FB;
 
 //
 //  PSF font header.
 //
-extern PSF_FONT_HDR             FontHdr;
+extern PSF_FONT_HDR  FontHdr;
 
 //
 //  Extended PSF font header.
 //
-extern PSF_FONT_HDR_EXTENDED    ExtFontHdr;
+extern PSF_FONT_HDR_EXTENDED  ExtFontHdr;
 
 //
 //  Contents of the current PSF file.
 //
-extern VOID                     *FontFile;
+extern VOID  *FontFile;
 
 //
 //  Size of the 4-KiB page bitmap.
 //
-extern UINTN                    BitmapSize;
+extern UINTN  BitmapSize;
 
 //
 //  A bitmap of 4-KiB aligned usable memory.
@@ -52,6 +52,6 @@ extern UINTN                    BitmapSize;
 //  Includes memory that is already in-use
 //  by a process in the kernel, as well.
 //
-extern MEMORY_BITMAP            *Bitmap;
+extern UINT8  Bitmap[2048];
 
 #endif /* KernRuntimeValues.h */
--- src/Kernel/Util/KernRuntimeValues.c
+++ src/Kernel/Util/KernRuntimeValues.c
@@ -10,19 +10,19 @@
 //
 //  Framebuffer related values
 //
-UINT32                   ScreenRow          = 1;
-UINT32                   ScreenCol          = 1;
-KERN_FRAMEBUFFER         *FB                = NULL;
+UINT32            ScreenRow = 1;
+UINT32            ScreenCol = 1;
+KERN_FRAMEBUFFER  *FB       = NULL;
 
 //
 //  Font related values
 //
-PSF_FONT_HDR             FontHdr;
-PSF_FONT_HDR_EXTENDED    ExtFontHdr;
-VOID                     *FontFile          = NULL;
+PSF_FONT_HDR           FontHdr;
+PSF_FONT_HDR_EXTENDED  ExtFontHdr;
+VOID                   *FontFile = NULL;
 
 //
 //  Bitmap related values
 //
-UINTN                    BitmapSize         = 0;
-MEMORY_BITMAP            *Bitmap            = NULL;
+UINTN  BitmapSize   = 0;
+UINT8  Bitmap[2048];
