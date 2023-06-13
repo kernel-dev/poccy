507efd95c9caba806ef23326a6a71296e53c5df8
kernel-dev <twitchisadeck@gmail.com>
2023-06-13 05:31:17 +0200

    refactor(KernAssert): add `__FILE__` macro to assert trace
    - add: write to serial output

--- src/Common/Assert/KernAssert.h
+++ src/Common/Assert/KernAssert.h
@@ -1,18 +1,29 @@
 #ifndef KERN_ASSERT_H
 #define KERN_ASSERT_H
 
+#include "../Drivers/IO/serial.h"
 #include "../Util/KernRuntimeValues.h"
 #include "../Graphics/KernText.h"
 
-#define STR_EXPR(expr) #expr
+#define STR_EXPR(expr)  #expr
 #define ASSERT(expr) \
     if (!(expr)) \
     { \
-        kprint ("ASSERT ( "); \
+        kprint ("["); \
+        kprint (__FILE__); \
+        kprint ("] ASSERT ( "); \
         kprint (STR_EXPR(expr)); \
         kprint (" )"); \
         kprint (" == "); \
         kprint (expr ? "1\n" : "0\n"); \
+        WriteSerialStr ("["); \
+        WriteSerialStr (__FILE__); \
+        WriteSerialStr ("] ASSERT ( "); \
+        WriteSerialStr (STR_EXPR(expr)); \
+        WriteSerialStr (" )"); \
+        WriteSerialStr (" == "); \
+        WriteSerialStr (expr ? "1\n" : "0\n"); \
+        for ( ; ;) { __asm__("hlt"); } \
     }
 
 #endif /* KernAssert.h */
