c9337e625176369cf7736870cff2ddd208509ab3
kernel-dev <twitchisadeck@gmail.com>
2023-06-14 19:12:53 +0200

    remove: unnecessary debug prints

--- src/Kernel/Kernel.c
+++ src/Kernel/Kernel.c
@@ -55,31 +55,12 @@ EfiMain (
   //
   kprint ("Hello, kernelOS!\n");
 
-  kprint ("Constructing bitmap memory map...\n");
-
   //
   //  Create an internal Bitmap
   //  representation of system memory.
   //
   KernCreateMMap (MemoryMap);
-
-  UINTN  Increment = 0;
-
-  for (UINTN Index = 0; Index < BitmapSize; Index++) {
-    for (UINTN Bit = 0; Bit < 8; Bit++) {
-      CHAR8  *Address = _KernItoa (
-                          (UINTN)(MemoryMap->MemoryMap->PhysicalStart + ((Index + Increment) * 4096))
-                          );
-
-      WriteSerialStr ("0x");
-      WriteSerialStr (Address);
-      WriteSerialStr (" : ");
-      WriteSerialStr (((Bitmap[Index] >> Bit) & 1) == 1 ? "Free\n" : "Unavailable\n");
-
-      Increment++;
-    }
-  }
-
+  
   //
   //  Should never reach here.
   //  Will be removed later.
