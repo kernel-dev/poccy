93842e9d354679c50e46cacf183a31edaac659d1
kernel-dev <twitchisadeck@gmail.com>
2023-08-28 16:58:35 +0200

    draft: PS2 keyboard driver

---
+++ src/Kernel/Drivers/PS2/PS2KeyboardDriver.c
@@ -0,0 +1,19 @@
+#include "Drivers/PS2/PS2KeyboardDriver.h"
+#include "Drivers/IO/serial.h"
+#include "Graphics/KernText.h"
+#include "Util/KernString.h"
+
+#include <Uefi.h>
+
+#include <Library/UefiLib.h>
+
+//
+//  TODO:   Implement `ReadKey()` method once interrupts
+//          are properly set up.
+//
+VOID
+ReadKey (
+  )
+{
+
+}
